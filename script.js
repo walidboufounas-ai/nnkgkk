c8ec2fc41073232685838e25b7cb8847b7dd2a55
walidboufounas-ai <walidboufounas@gmail.com>
2026-05-16 16:36:29 +0100

    ok

--- script.js
+++ script.js
@@ -6,27 +6,39 @@ const password = document.getElementById("password");
 
 btn.addEventListener("click", function () {
 
-    // Condition NOM en majuscule
+    
     if (nom.value !== nom.value.toUpperCase()) {
         alert("Le nom doit être en MAJUSCULE !");
         return;
     }
 
-    // Condition EMAIL
+    
     if (!email.value.includes("@gmail.com")) {
         alert("L'email doit contenir @gmail.com !");
         return;
     }
 
-    // Conditions PASSWORD
+    
     let hasUppercase = /[A-Z]/.test(password.value);
     let hasLetter = /[a-zA-Z]/.test(password.value);
     let hasNumber = /[0-9]/.test(password.value);
 
     if (!hasUppercase || !hasLetter || !hasNumber) {
-        alert("Le mot de passe doit contenir : une majuscule, des lettres et des chiffres !");
+        alert("Le mot de passe doit contenir une majuscule, des lettres et des chiffres !");
         return;
     }
+
+  
+    const user = {
+        name: nom.value,
+        email: email.value,
+        password: password.value
+    };
+
+    
+    localStorage.setItem("user", JSON.stringify(user));
+
+    console.log(user);
 
     alert("Bonne inscription !");
 });
