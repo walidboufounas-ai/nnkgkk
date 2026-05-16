bbf8b1812ae6d4b567d7adbbfa2e93add6fbb4de
walidboufounas-ai <walidboufounas@gmail.com>
2026-05-16 16:26:13 +0100

    first commit

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-console.log("Script loaded successfully!");
 const btn = document.getElementById("btn");
-const nom = document.getElementById("nom");
+
+const nom = document.getElementById("name");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 
-console.log(btn);
-console.log(nom.value);
-console.log(email.value);
-console.log(password.value);
-btn.addEventListener("click", function() {
-    console.log("Button clicked!");
-    console.log("Nom: " + nom.value);
-    console.log("Email: " + email.value);
-    console.log("Password: " + password.value);
-    alert("bonne inscription");
+btn.addEventListener("click", function () {
+
+    // Condition NOM en majuscule
+    if (nom.value !== nom.value.toUpperCase()) {
+        alert("Le nom doit être en MAJUSCULE !");
+        return;
+    }
+
+    // Condition EMAIL
+    if (!email.value.includes("@gmail.com")) {
+        alert("L'email doit contenir @gmail.com !");
+        return;
+    }
+
+    // Conditions PASSWORD
+    let hasUppercase = /[A-Z]/.test(password.value);
+    let hasLetter = /[a-zA-Z]/.test(password.value);
+    let hasNumber = /[0-9]/.test(password.value);
+
+    if (!hasUppercase || !hasLetter || !hasNumber) {
+        alert("Le mot de passe doit contenir : une majuscule, des lettres et des chiffres !");
+        return;
+    }
+
+    alert("Bonne inscription !");
 });
